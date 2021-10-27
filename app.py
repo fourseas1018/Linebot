a79dc265887f325d2282842457be7d62d1d04f0e
fourseas1018 <73520473+fourseas1018@users.noreply.github.com>
2021-10-27 19:05:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 #載入LineBot所需要的套件
 from flask import Flask, request, abort
+import time
 import pandas as pd
 import yahoo_fin.stock_info as si
 import numpy as np
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import  InvalidSignatureError
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import ( 
+    InvalidSignatureError
+)
 from linebot.models import *
+import schedule
 import datetime
 import re
+
 
 app = Flask(__name__)
 
@@ -18,8 +25,8 @@ handler = WebhookHandler('223cbe254528bf518e42358505c97bdb')
 
 line_bot_api.push_message('U9e6f525ecd8275e7d12127dbad547c79', TextSendMessage(text='你可以開始了'))
 
-
-
+uspric = pd.DataFrame(columns=['stock','bs','print', 'date_info','type'])
+yourid = 'U9e6f525ecd8275e7d12127dbad547c79'
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -43,18 +50,40 @@ def callback():
 def write_user_stock_function(stock,bs,price,uspric):
     df=pd.DataFrame([(stock,bs,price,datetime.datetime.utcnow(),'care_stock')],columns=['stock','bs','price', 'date_info','type'])
     uspric=uspric.append(df)
+    uspric.reset_index(inplace=True, drop=True)
     return uspric
-    
 #--------------刪除使用者的股票--------------
 def delete_user_stock_function(stock,uspric):
     uspric.drop(index=uspric.index[np.where(uspric['stock']==stock)[0]],inplace=True)
     return uspric
-def show_user_stock_founction(uspric):
-    cel=list(uspric[np.where(uspric['type']=='care_stock')])
-    return cel
+#--------------秀出使用者的股票--------------
+def job():
+    fliter=uspric[uspric['type']=='care_stock']
+    for index,row in fliter.iterrows():
+        stock=row['stock']
+        bs=row['bs']
+        price=float(row['price']) 
+        nowstock=si.get_live_price(str(row['stock'])+'.TW')
+        if bs == '<'or'＜':
+            if float(nowstock) < price:
+                get=str(stock)+'的價格已經低於'+str(price)+'\n'+str(stock)+'價格為:'+str(nowstock)
+                line_bot_api.push_message(yourid,TextSendMessage(text=get))
+        elif bs == '＞'or'>':
+            if float(nowstock) > price:
+                get=str(stock)+'的價格已經超過'+str(price)+'\n'+str(stock)+'價格為:'+str(nowstock)
+                line_bot_api.push_message(yourid,TextSendMessage(text=get))
+        else :
+            if float(nowstock) == price:
+                get=str(stock)+'的價格抵達'+str(price)+'\n'+str(stock)+'價格為:'+str(nowstock)
+                line_bot_api.push_message(yourid,TextSendMessage(text=get))
+    second_5_j = schedule.every(10).seconds.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
-uspric = pd.DataFrame(columns=['stock','bs','print', 'date_info','type'])
+
+
 #訊息傳遞區塊
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -64,7 +93,7 @@ def handle_message(event):
     uid = profile.user_id
     usespeak = str(event.message.text)
     #判斷是否為要儲存的資料
-    if re.match('[0-9]{4}[<>][0-9]',usespeak):
+    if re.match('[0-9]{4}[<>=＜＞＝][0-9]',usespeak):
         write_user_stock_function(stock=usespeak[0:4],bs=usespeak[4:5],price=usespeak[5:],uspric=uspric)
         line_bot_api.push_message(uid,TextSendMessage(usespeak[0:4]+'這支股票已經儲存進關注清單'))
         return 0
